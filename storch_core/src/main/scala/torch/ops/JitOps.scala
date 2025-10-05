e6971418fe68031f0867f0f3f673f289b6824a5f
Muller Helen <hai710459649@gmail.com>
2025-10-05 17:04:01 +0800

    clean code  add torch.jit object

--- storch_core/src/main/scala/torch/ops/JitOps.scala
+++ storch_core/src/main/scala/torch/ops/JitOps.scala
@@ -5,7 +5,6 @@ import org.bytedeco.javacpp.{BytePointer, Pointer, PointerPointer}
 import org.bytedeco.pytorch.global.torch as torchNative
 import org.bytedeco.pytorch.{Tensor as PytorchTensor, *}
 import torch.nn.modules.TensorModule
-
 import java.io.{ByteArrayOutputStream, InputStream, ObjectOutputStream}
 import java.nio.ByteBuffer
 import scala.collection.mutable
@@ -14,6 +13,229 @@ import torch.internal.NativeConverters.{fromNative, toScalar}
 
 trait JitOps {
 
+  object jit {
+
+    def compile[ParamType <: FloatNN | ComplexNN : Default](
+                                                             model: TensorModule[ParamType]
+                                                           ): CompilationUnit =
+      val bytes = modelToBytes(model)
+      val bytePointer = new BytePointer(ByteBuffer.wrap(bytes))
+      torchNative.compile(bytePointer)
+
+    def compile[ParamType <: FloatNN | ComplexNN : Default](
+                                                             model: TensorModule[ParamType],
+                                                             modelPath: String
+                                                           ): CompilationUnit =
+      val archive = new OutputArchive
+      model.save(archive)
+      archive.save_to(modelPath)
+      val compileUnit = torchNative.compile(modelPath)
+      compileUnit
+
+    def freeze(scriptModule: JitModule): JitModule = torchNative.freeze(scriptModule)
+
+    def freeze(
+                scriptModule: JitModule,
+                preservedAttrs: Seq[String],
+                optimizeNumerics: Boolean = true
+              ): JitModule = {
+      val strVector = new StringVector(preservedAttrs *)
+      val opt = new StringVectorOptional(strVector)
+      torchNative.freeze(scriptModule, opt, optimizeNumerics)
+    }
+
+
+    def load(
+              modelPath: String,
+              device: String,
+              extrafilesMap: ExtraFilesMap,
+              weightsOnly: Boolean
+            ): JitModule = {
+      val deviceOpt: DeviceOptional = new DeviceOptional(new Device(device))
+      torchNative.load(modelPath, deviceOpt, extrafilesMap, weightsOnly)
+    }
+
+    def load(
+              modelStream: ReadAdapterInterface,
+              device: String,
+              extrafilesMap: ExtraFilesMap,
+              weightsOnly: Boolean
+            ): JitModule = {
+      val deviceOpt: DeviceOptional = new DeviceOptional(new Device(device))
+      torchNative.load(modelStream, deviceOpt, extrafilesMap, weightsOnly)
+    }
+
+    def from_pretrain_model(modelFilePath: String): JitModule = torchNative.load(modelFilePath)
+
+    def from_pretrain_model(modelPath: String, device: String, weightsOnly: Boolean): JitModule = {
+      val deviceOpt: DeviceOptional = new DeviceOptional(new Device(device))
+      torchNative.load(modelPath, deviceOpt, weightsOnly)
+    }
+
+    def from_pretrain_model(
+                             modelPath: String,
+                             device: String,
+                             extrafilesMap: ExtraFilesMap,
+                             weightsOnly: Boolean
+                           ): JitModule = {
+      val deviceOpt: DeviceOptional = new DeviceOptional(new Device(device))
+      torchNative.load(modelPath, deviceOpt, extrafilesMap, weightsOnly)
+    }
+
+    def getFusionStrategy(): FusionStrategy = torchNative.getFusionStrategy()
+
+    def setFusionStrategy(fusionStrategy: FusionStrategy): FusionStrategy =
+      torchNative.setFusionStrategy(fusionStrategy)
+
+    def isBlockListedSchema(funcSchema: FunctionSchema): Boolean =
+      torchNative.isBlockListedSchema(funcSchema)
+
+    def parseSchema(layerName: String, bool: Boolean): FunctionSchema =
+      torchNative.parseSchema(layerName, bool)
+
+    def parseSchema(layerName: String): FunctionSchema = torchNative.parseSchema(layerName)
+
+    def optimize_for_inference(scriptModule: JitModule): JitModule =
+      torchNative.optimize_for_inference(scriptModule)
+
+    def unpackOutputs(vecSeq: ValueVector): ValueVector = torchNative.unpackOutputs(vecSeq)
+
+    def findAllNodes(graph: Graph, symbol: Symbol, bool: Boolean) =
+      torchNative.findAllNodes(graph, symbol, bool)
+
+    def findAllNodes(block: Block, symbol: Symbol, bool: Boolean) =
+      torchNative.findAllNodes(block, symbol, bool)
+
+    def findAllNodes(blockSeq: Seq[Block], symbol: Symbol, bool: Boolean) = {
+      val ref = new BlockArrayRef()
+      blockSeq.foreach(block => ref.put(block))
+      torchNative.findAllNodes(ref, symbol, bool)
+    }
+
+    def replaceBlockWithFallbackGraph(block: Block, vec: ValueVector): JitNode = {
+
+      torchNative.replaceBlockWithFallbackGraph(block, vec)
+    }
+
+    def parse_and_initialize_jit_module(
+                                         modelStream: ByteBuffer,
+                                         size: Long,
+                                         extraFilesMap: ExtraFilesMap
+                                       ): JitModule = {
+      torchNative.parse_and_initialize_jit_module(modelStream, size, extraFilesMap)
+
+    }
+
+    def load_jit_module_from_file(fileName: String, extraFilesMap: ExtraFilesMap): JitModule = {
+
+      torchNative.load_jit_module_from_file(fileName, extraFilesMap)
+
+    }
+
+    def load_jit_module_from_stream(
+                                     inputStream: Pointer,
+                                     extraFilesMap: ExtraFilesMap,
+                                     device: Option[String] = None
+                                   ): JitModule = {
+
+      //    val streamPointer: Pointer = None
+      //    device match {
+      //      case None => torchNative.load_jit_module_from_stream(streamPointer ,extraFilesMap)
+      //    }
+      //    val deviceOpt:DeviceOptional = new DeviceOptional(new Device(device))
+      torchNative.load_jit_module_from_stream(inputStream, extraFilesMap)
+    }
+
+    def LintGraph(graph: Graph): Unit = torchNative.LintGraph(graph)
+
+    def insertGraph(
+                     graph: Graph,
+                     insertGraph: Graph,
+                     vec: ValueVector,
+                     map: ValueValueMap
+                   ): ValueVector = {
+      torchNative.insertGraph(graph, insertGraph, vec)
+    }
+
+    def insertGraph(graph: Graph, insertGraph: Graph, vec: ValueVector): ValueVector = {
+      torchNative.insertGraph(graph, insertGraph, vec)
+    }
+
+    def getAllSortedOperatorsFor(symbol: Symbol): OperatorVector = {
+
+      val operatorVector = torchNative.getAllSortedOperatorsFor(symbol)
+      operatorVector
+    }
+
+    def findSimilarOperators(symbol: Symbol): SymbolVector = {
+      torchNative.findSimilarOperators(symbol)
+    }
+
+    def matchSchema(
+                     funcSchema: FunctionSchema,
+                     sourceRange: SourceRange,
+                     graph: Graph,
+                     nameValueSeq: Seq[NamedValue],
+                     nameValueSeq2: Seq[NamedValue]
+                   ): MatchedSchema = {
+      val ref = new NamedValueArrayRef()
+      nameValueSeq.foreach(nv => ref.put(nv))
+      val ref2 = new NamedValueArrayRef()
+      nameValueSeq2.foreach(nv => ref2.put(nv))
+      torchNative.matchSchema(funcSchema, sourceRange, graph, ref, ref2)
+    }
+
+    def matchSchemas(
+                      funcSchemaSeq: Seq[FunctionSchema],
+                      sourceRange: SourceRange,
+                      graph: Graph,
+                      nameValueSeq: Seq[NamedValue],
+                      nameValueSeq2: Seq[NamedValue]
+                    ): SizeTMatchedSchemaPair = {
+      val fsVec = new FunctionSchemaVector()
+      for (elem <- funcSchemaSeq) {
+        fsVec.put(elem)
+      }
+      val ref = new NamedValueArrayRef()
+      nameValueSeq.foreach(nv => ref.put(nv))
+      val ref2 = new NamedValueArrayRef()
+      nameValueSeq2.foreach(nv => ref2.put(nv))
+      torchNative.matchSchemas(fsVec, sourceRange, graph, ref, ref2)
+    }
+
+    def emitBuiltinCall(
+                         sourceRange: SourceRange,
+                         graph: Graph,
+                         symbol: Symbol,
+                         nameValueSeq: Seq[NamedValue],
+                         nameValueSeq2: Seq[NamedValue]
+                       ): Value = {
+      val ref = new NamedValueArrayRef()
+      nameValueSeq.foreach(nv => ref.put(nv))
+      val ref2 = new NamedValueArrayRef()
+      nameValueSeq2.foreach(nv => ref2.put(nv))
+      torchNative.emitBuiltinCall(sourceRange, graph, symbol, ref, ref2)
+    }
+
+    def optimize_for_inference(scriptModule: JitModule, parameterSeq: Seq[String]): JitModule = {
+      val strVector = new StringVector(parameterSeq *)
+      torchNative.optimize_for_inference(scriptModule, strVector)
+    }
+
+    def getWriteableTensorData(tensor: PytorchTensor, bool: Boolean): WriteableTensorData =
+      torchNative.getWriteableTensorData(tensor, bool)
+
+    def findOperatorFor(operatorName: String, overloadName: String): Operator = {
+      val opname = new BytePointer(operatorName)
+      val ovname = new BytePointer(overloadName)
+      import org.bytedeco.pytorch.OperatorName
+      val operatorNamez: OperatorName = new OperatorName(opname, ovname)
+      val operator = torchNative.findOperatorFor(operatorNamez)
+      operator
+    }
+
+
+  }
   def compile[ParamType <: FloatNN | ComplexNN: Default](
       model: TensorModule[ParamType]
   ): CompilationUnit =
